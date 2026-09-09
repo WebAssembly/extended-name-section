8f3cd880c4135cd5d283909fa7919b77dc2a130e
bvisness <bvisness@users.noreply.github.com>
2026-09-09 18:29:40 +0000

    deploy: e16844678adb262a692f1dfa3ebe22db5deb9d46

--- core/_static/documentation_options.js
+++ core/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '3.0 + extended-name-section (2026-09-01)',
+    VERSION: '3.0 + extended-name-section (2026-09-09)',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
